9b7a0e3fdf6edddb48576b6b85845497398cf31b
pyang55 <perry.yang@logicmonitor.com>
2017-09-19 10:23:38 -0700

    renamed user agent

--- logicmonitor/config.go
+++ logicmonitor/config.go
@@ -21,7 +21,7 @@ func (c *Config) NewLMClient() (*lmv1.DefaultApi, error) {
 		},
 	}
 	config.BasePath = "https://" + c.Company + ".logicmonitor.com/santaba/rest"
-	config.UserAgent = "terraform-provider-logicmonitor"
+	config.UserAgent = "LogicMonitor Terraform Provider"
 
 	api := lmv1.NewDefaultApi()
 	api.Configuration = config
